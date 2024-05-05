6f72bc331bd783aa4d372eb770b7f0f516f5dcb7
Dan Hsu <hsu86110@gmail.com>
2024-05-05 20:31:51 +0800

    feature: connect user balance

--- src/components/containers/basicContainer.tsx
+++ src/components/containers/basicContainer.tsx
@@ -1,13 +1,34 @@
 import BasicDataField from "../fields/basicDataField";
 import BasicInputField from "../fields/basicInputField";
 import ActionButton from "../buttons/actionButton";
-import { useState } from "react";
+import { useContext, useMemo, useState } from "react";
+import { useSuiClientQuery } from "@mysten/dapp-kit";
+import { AppContext } from "@/context/AppContext";
 
 const BasicContainer = () => {
+  const { walletAddress, suiName } = useContext(AppContext);
+  const { data: suiBalance } = useSuiClientQuery("getBalance", {
+    owner: walletAddress ?? "",
+  });
   const [selectedToken, setSelectedToken] = useState<string>("SUI");
+
+  const userBalance = useMemo(() => {
+    if (suiBalance?.totalBalance) {
+      return Math.floor(Number(suiBalance?.totalBalance) / 10 ** 9);
+    } else {
+      return 0;
+    }
+  }, [suiBalance]);
+
   return (
     <div className="w-[80%] flex flex-col items-center justify-center gap-4">
-      <BasicDataField label="Your Wallet Balance" value="0.0000" unit="SUI" />
+      <BasicDataField
+        label="Your Wallet Balance"
+        value={userBalance ?? "0.0000"}
+        spaceWithUnit
+        unit="SUI"
+        minFractionDigits={0}
+      />
       <BasicInputField
         label="Input"
         inputValue="0.0000"
